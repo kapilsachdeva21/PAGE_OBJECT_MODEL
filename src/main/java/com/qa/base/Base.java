4e9fce8c8d532d10e3566b53029257fd3e171f15
kapil.sachdeva <kapil.sachdeva@u2opiamobile.com>
2018-12-16 05:29:55 +0800

    Merge change (Home Page test)

--- src/main/java/com/qa/base/Base.java
+++ src/main/java/com/qa/base/Base.java
@@ -1,7 +1,52 @@
 package com.qa.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 public class Base{
 
-    public static void main(String arg[]){
-    System.out.println("hello");
-}}
+ public static WebDriver driver;
+ public static Properties prop;
+
+ public Base()
+ {
+     try {
+         prop = new Properties();
+         FileInputStream fi = new FileInputStream("E:\\PAGE_OBJECT_MODEL\\src\\main\\java\\com\\qa\\config\\config.properties");
+         prop.load(fi);
+     }catch (IOException e)
+     {
+         e.printStackTrace();
+     }
+
+ }
+
+ public static void init()
+ {
+     String browserName=prop.getProperty("browser");
+     if(browserName.equalsIgnoreCase("chrome"))
+     {
+         System.setProperty("webdriver.chrome.driver","E:\\driver\\chromedriver.exe");
+         driver=new ChromeDriver();
+     }
+     else if(browserName.equalsIgnoreCase("firefox"))
+     {
+         driver=new FirefoxDriver();
+     }
+
+     driver.manage().window().maximize();
+     driver.manage().deleteAllCookies();
+     driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+     driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+     driver.get(prop.getProperty("url"));
+
+
+ }
+
+}
